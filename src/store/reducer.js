b5ea6d6125b2edbbaf5afea89069b002baa9aeae
Travis Overby <tek62441@adobe.com>
2019-06-25 15:26:49 -0700

    Dispatch actions to reducers from Counter component

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,52 @@
 const initialState = {
-  counter: 0
-};
+    counter: 0,
+    results: []
+}
 
 const reducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    case 'DECREMENT':
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    case 'ADD':
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+    case 'SUBTRACT':
+      return {
+        ...state,
+        counter: state.counter - action.value
+      };
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({ id: Number(new Date()), value: state.counter })
+      };
+    case 'DELETE_RESULT':
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1);
+      const updatedArray = state.results.filter(result => result.id !== action.resultId);
+      console.log(updatedArray);
+      return {
+        ...state,
+        results: updatedArray
+      };
+
+      default:
+        return {
+          ...state
+        };
+  }
+
 };
 
 export default reducer;
